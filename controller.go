f7658e45b3d0db460bef3b13c04b5cf60e8e8f7f
Luigi Tanzini <tanzini.luigi@gmail.com>
2019-02-03 20:29:27 +0100

    added convenience method to retrieve the json map from inputs

--- controller.go
+++ controller.go
@@ -1,10 +1,14 @@
 package mage
 
 import (
+	"encoding/json"
+	"errors"
 	"golang.org/x/net/context"
 )
 
 type RequestInputs map[string]requestInput
+
+var ErrMageNoJSON = errors.New("inputs do not contain json data")
 
 func InputsFromContext(ctx context.Context) RequestInputs {
 	inputs := ctx.Value(KeyRequestInputs).(RequestInputs)
@@ -18,4 +22,26 @@ type Controller interface {
 	Process(ctx context.Context, out *ResponseOutput) Redirect
 	//called to release resources
 	OnDestroy(ctx context.Context)
+}
+
+// Convenience method to recover all json inputs
+// Returns user json inputs as a map string -> interface{}
+func ParseJSONInputs(ctx context.Context) (map[string]interface{}, error) {
+	inputs := InputsFromContext(ctx)
+	if inputs == nil {
+		return nil, ErrMageNoJSON
+	}
+
+	jin := []byte(inputs[KeyRequestJSON].Value())
+
+	var data interface{}
+	err := json.Unmarshal(jin, &data)
+
+	if err != nil {
+		return nil, err
+	}
+
+	d := data.(map[string]interface{})
+
+	return d, nil
 }
